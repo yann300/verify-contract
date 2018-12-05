7c3731ea029e19382b636c612f755005c75e15df
mulili <mulili.nzuki@gmail.com>
2018-12-06 00:57:34 +0300

    added the api calls to etherscan

--- README.md
+++ README.md
@@ -1,19 +1,23 @@
-# remix-plugin
-Contains everything you need to start coding a remix plugin
 
-## how to get started
+# Contract Verification Plugin
+The contract-verification-plugin integrates remix with the ability to verify contracts deployed from the remix-ide, with etherscan. The sooner a solidity developer learns of a smart contract deployment issue, the sooner they can understand it and make a correct fix.
+The remix plugin API is based from https://github.com/yann300/remix-plugin
 
-git clone https://github.com/ethereum/remix-ide
 
-Serve the `src` folder through a web server `http-server .`
+## Usage
 
-Go in Remix ( https://remix.ethereum.org / https://remix-alpha.ethereum.org ) / settings tab, under the `Plugin` section paste the following declaration:
-
+* Use http://remix-alpha.ethereum.org/
+* Go to Settings, Under plugins fill out this info:
 ```
 {
-    "title": "name_of_plugin",
-    "url": "http://127.0.0.1:__port__"
+    "title": "Contract Verification Plugin",
+    "url": "http://remix-contract-verification.surge.sh"
 }
 ```
+* Click the Load button and you should see `Contract Verification Plugin` button appear
+* Click the `Contract Verification Plugin` button to load the plugin into a small window
+* Now click on the button you would like to perform the analysis
 
-Then start the plugin by licking on its icon.
+
+## Disclaimer
+By using this plugin, you acknowledge that the compilation results from your contract will be sent over basic HTTP to a 3rd party server for analysis. And that the developers of the this plugin ARE NOT LIABLE to any LOSS, INJURY, HACKING , DATA LOSS, FINANCIAL LOSS, TIME LOSS and DATA CORRUPTION that may occur from your use of our plugin. You also acknowledge that this plugin is provided on an as-is basis and all functionality works as documented above.
---
+++ src/assets/js/plugin.js
@@ -0,0 +1,97 @@
+var extension = new window.RemixExtension();
+var compileMsg = "Compiling smart contract, please wait...";
+
+async function do_post(data, cb) {
+  var etherscanApi = "https://api.etherscan.io/api";
+  var contractData = data[0];
+  var data = {
+      apikey: '2GTVV7HHTK8CCG2NFK5NRXYPY6DKINJH48',                     //A valid API-Key is required        
+      module: 'contract',                             //Do not change
+      action: 'verifysourcecode',                     //Do not change
+      contractaddress: $('#contractaddress').val(),   //Contract Address starts with 0x...     
+      sourceCode: contractData.sources[contractData.sources.target].content,             //Contract Source Code (Flattened if necessary)
+      contractname: contractData.sources.target.split("/").pop(),         //ContractName
+      compilerversion: $('#compilerversion').val(),   // see http://etherscan.io/solcversions for list of support versions
+      optimizationUsed: $('#optimizationUsed').val(), //0 = Optimization used, 1 = No Optimization
+      runs: 200,                                      //set to 200 as default unless otherwise         
+      constructorArguements: $('#constructorArguements').val(),   //if applicable
+  };
+  try{
+    const response =  await fetch(analysisServerUrl,{ method: 'POST', headers: { "Content-Type": "application/json; charset=utf-8"},body: JSON.stringify(data)});
+    console.log(response);
+    cb(response);
+
+  }
+  catch (error) {
+    console.log(error);
+  }
+ 
+}
+
+
+async function do_get(data, cb) {
+
+  try{
+    const response =  await (await fetch(analysisServerUrl,{ method: 'POST', headers: { "Content-Type": "application/json; charset=utf-8"},body: JSON.stringify(data)})).json();
+
+    cb(response);
+
+  }
+  catch (error) {
+    console.log(error);
+  }
+ 
+}
+
+function handleCompileSuccess(result) {
+  if(result[0] === null){
+     document.querySelector('div#results').innerHTML = `No compile results found for this contract, please make sure <br> the contract compiles correctly.`;
+     return;
+  }
+  document.querySelector('div#results').innerHTML = `Verifying contract. Please wait...`;
+  // fetch results
+  do_post(result, function(res) {
+
+        document.querySelector('div#results').innerHTML = res['output'];
+    
+  });
+   
+
+}
+
+function handleCompileFailure(error) {
+  document.querySelector('div#results').innerHTML = error;
+}
+
+
+
+window.onload = function () {
+  console.log("LOADED CONTRACT VERIFICATION PLUGIN");
+  extension.listen('compiler', 'compilationFinished', function (result) {
+    console.log("GOT A COMPILE RESULT: ");
+    console.log(result);
+  });
+
+  extension.listen('txlistener', 'newTransaction', function (transaction) {
+    console.log("GOT A TRANSACTION ");
+    console.log(transaction);
+  });
+
+
+
+  document.querySelector('input#verify-contract').addEventListener('click', function () {
+    var div = document.querySelector('div#results');
+    div.innerHTML = compileMsg;
+    extension.call('compiler', 'getCompilationResult', [], function (error, result ) {
+      console.log(result);
+        if(result) {
+          handleCompileSuccess(result);
+        }
+        else{
+          handleCompileFailure(error);
+        }
+
+    });
+  });
+
+}
